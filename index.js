010c8126316648129371f702fff78df3c8258246
Ian July <ianjuly4@gmail.com>
2023-07-15 12:43:15 -0600

    cats array lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+   let catNames = [...cats, "Broom"];
+   return catNames;
+}
+function prependCat() {
+    let nameCats = ["Arnold", ...cats];
+    return nameCats;
+}
+function removeLastCat(){
+    let copysOfCats = cats.slice(0,-1);
+    return copysOfCats;
+}
+function removeFirstCat(){
+    let catsOfCopys = cats.slice(1);
+    return catsOfCopys;
+}
